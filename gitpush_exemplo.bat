b082c359277c80e1e4a4d011cca173f58d6c40f4
rfdouro <rfdouro@gmail.com>
2025-02-23 08:51:59 -0300

    ajuste exemplo gitpush 2025_02_23_ 8:51

--- gitpush_exemplo.bat
+++ gitpush_exemplo.bat
@@ -1,13 +1,19 @@
+set now=%date:~6,4%_%date:~3,2%_%date:~0,2%_%time:~0,2%:%time:~3,2%
+echo.%now%
+
+
 if exist .git\ (
  echo Ok
 ) else (
  git init
 )
 
+git config user.name "USUARIOGITHUB"
+git config user.email "NOME@MAIL.com"
+
 @echo off
 set /p messComit=Qual a mensagem de commit?: 
-echo ok %dmessComit%
 
 git add -A
-git commit -m "codigo: PUSH %messComit%"
+git commit -m "%messComit% %now%"
 git push -f https://<TOKEN>@github.com/<USUARUO>/<REPOSITORIO>.git master
